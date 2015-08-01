e3ca87f6873be46a84f560f0ae9554f89cbd1edc
Marcus Rosti <mer3ef@virginia.edu>
2015-08-01 16:43:34 -0400

    cleaned up source folder

--- src/controller.py
+++ src/controller.py
@@ -1,20 +1,20 @@
-import common_lib.py
+#import common_lib.py
 import logging
 
-logging.basicConfig(filename='../logs/controller.log',level=logging.DEBUG)
+logging.basicConfig(filename='../logs/controller.log',level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
-if __name__ == "__main__":
-    main()
-def main()
+def main():
     logging.debug('setting filename to ../data/example.csv')
     filename = "../data/example.csv"
 
-    dates = common_lib.update(filename))
+#    dates = common_lib.update(filename))
     logging.debug('recieved dates from date function')
     
-    extracted_json = common_lib.extract(dates)
-    loggin.debug('received dates from extract funciton')
+#    extracted_json = common_lib.extract(dates)
+    logging.debug('received dates from extract funciton')
     
-    common_lib.update_csv(extracted_json)
-    logging.debut('csv was successfully updated')
+#    common_lib.update_csv(extracted_json)
+    logging.debug('csv was successfully updated')
 
+if __name__ == "__main__":
+        main()
