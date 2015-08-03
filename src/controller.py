12d5990d53d4ce528c2bb86624bc103595627aa0
Marcus Rosti <mer3ef@virginia.edu>
2015-08-03 12:44:38 -0400

    Combined files into common lib and updated loggign

--- src/controller.py
+++ src/controller.py
@@ -1,20 +1,25 @@
 #import common_lib.py
 import logging
+import common_lib
 
-logging.basicConfig(filename='../logs/controller.log',level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-
+#intialize logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='../logs/controller.log',level=logging.DEBUG, \
+    format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    
 def main():
-    logging.debug('setting filename to ../data/example.csv')
-    filename = "../data/example.csv"
+    logger.debug('setting filename to ../data/testOutput.csv')
+    filename = "../data/testOutput.csv"
 
-#    dates = common_lib.update(filename))
-    logging.debug('recieved dates from date function')
-    
+    dates = common_lib.update(filename)
+    logger.debug(dates)
+    logger.debug('recieved dates from date function')
+
 #    extracted_json = common_lib.extract(dates)
-    logging.debug('received dates from extract funciton')
-    
-#    common_lib.update_csv(extracted_json)
-    logging.debug('csv was successfully updated')
+    logger.debug('received dates from extract funciton')
+
+    common_lib.parseJSONFile('../data/example.json')
+    logger.debug('csv was successfully updated')
 
 if __name__ == "__main__":
         main()
