496d333c56de90f15e1874e41024764597d35e9c
Nathan Harmon <imppilot@gmail.com>
2015-08-04 13:19:59 -0400

    Changed Update so that outputs zeros if times are within an hour of each other. Added gapFiller method to common_lib.

--- src/common_lib.py
+++ src/common_lib.py
@@ -12,9 +12,8 @@ logging.basicConfig(filename='../logs/controller.log',level=logging.DEBUG, \
 
 def update(filename):
     logger.debug('Updating dates for file: '+filename)
-    #returns last date and current date in unix time
+    #returns last date and current date in unix time, zeros if within an hour
 
-    #pull the last date from the file
     #reads in all the times and finds the max
     times = []
     with open(filename) as csvfile:
@@ -29,8 +28,41 @@ def update(filename):
 
     logger.debug('Returning \n\tfirst: ' + str(start_time) + \
                            '\n\tlast:  ' + str(end_time))
-    return start_time, end_time
 
+    #check to see if the times are an hour apart or more
+    if end_time-start_time > 3600:
+        return start_time, end_time
+    else:
+        return 0,0
+
+def gapFiller(filename):
+    #Finds the largest gap in the record
+    #returns the first and last date on either side of the gap, in unix time
+    #Returns zeros  if nothing larger than an hour
+        
+    import csv
+    #reads in all the times
+    times = [] 
+    with open(filename) as csvfile:
+        lines = csv.reader(csvfile)
+        next(lines, None) #skips the header
+        for line in lines:
+            times.append(int(line[0]))
+    #sort the times
+    times.sort()
+    #Itterate over the loop, finding the max gap and position
+    i = j = g = 0
+    while i < len(times)-1:
+        if times[i+1] - times[i] > g:
+            j = i
+            g = times[i+1]-times[i]
+        i += 1
+    
+    #See if the gap is larger than an hour
+    if g <= 3600:
+        return 0, 0
+    else:
+        return times[j], times[j+1]
 
 def parseJSONFile(JSONFile, output_file):
     logger.debug('Parsing file: ' + JSONFile)
