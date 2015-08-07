e349bd630791c6968fd037c116cd3384e237c408
Nikhil <nikhil.mascarenhas@gmail.com>
2015-08-07 13:14:49 -0400

    start_up() function partially completed.

--- src/user_interface_lib.py
+++ src/user_interface_lib.py
@@ -7,20 +7,32 @@
 
 import controller
 import logging
+import pandas as pd
 
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(filename='../logs/user_interface.log', level=logging.DEBUG, \
     format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
-weather_data_frame = ''
+
+WEATHER_DATA = None
 
 def start_up(data_file):
     """ Sets up data frame
-
         start_up takes in a datafile and spits out a global data frame that
             each of the subfunctions will use
     """
-    controller.run_update_process(filename)
+    
+    LOGGER.debug("Updating data file with latest values.")
+    controller.run_update_process(data_file)
+    LOGGER.debug("Update complete");
+
+    LOGGER.debug("Reading data file into Pandas Data Frame")
+    try:
+        global WEATHER_DATA 
+        WEATHER_DATA = pd.read_csv(data_file)
+        LOGGER.debug("Data Frame created")
+    except:
+        return False
     return True
 
 def option_1():
@@ -42,3 +54,7 @@ def option_4():
     """ Returns a string that does...
     """
     return "write your own function!"
+
+#test code TODO Delete this:
+start_up("../data/SampleCSV.csv")
+print(WEATHER_DATA)
